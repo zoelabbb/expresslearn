e64ab5aed3292f8a5213954e61804945f845fa8f
Ryu <zoelabbb@gmail.com>
2023-12-22 01:11:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express') // calling express
 const app = express() // creating express app
 const port = 3000 // defining port
+const postRouter = require('./routes/posts') // Import post router
+const bodyParser = require('body-parser') //import body parser
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+// use post router
+app.use('/api/posts', postRouter)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 
 // listening on port 3000
 app.listen(port, () => {
