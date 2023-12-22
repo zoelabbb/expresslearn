0ec402b10acfd23c6058e04603f999f6ccce193f
Ryu <zoelabbb@gmail.com>
2023-12-23 00:37:34 +0700

    Import dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express') // calling express
+require('dotenv').config() // Import .env file
+const port = process.env.APP_PORT
 const app = express() // creating express app
-const port = 3000 // defining port
 const postRouter = require('./routes/posts') // Import post router
 const bodyParser = require('body-parser') //import body parser
 const cors = require('cors') //import cors
@@ -15,7 +16,7 @@ app.use('/api/posts', postRouter) // use post router
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()) // parse application/json
 
-// listening on port 3000
+// listening on port from env file
 app.listen(port, () => {
-    console.log(`Your app listening at http://localhost:${port}`)
+    console.log(`Your app listening at http://localhost:${process.env.APP_PORT}`)
 })
